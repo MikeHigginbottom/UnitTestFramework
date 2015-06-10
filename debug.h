a2636dc5e839bb67d153c5cafd271aa151610a78
Mike Higginbottom <mike@mikehigginbottom.com>
2015-06-10 20:33:14 +0100

    Added pointer assertions

--- debug.h
+++ debug.h
@@ -7,6 +7,8 @@
 
 #define explicit_errno() (errno == 0 ? "No error" : strerror(errno))
 #define assrt(condition, msg, ...) if (!(condition)) { err(msg, ##__VA_ARGS__); errno = 0; goto error; }
+#define assrt_ptr(ptr) if (!(ptr)) { err("Memory failure - pointer was NULL"); errno = 0; goto error; }
+#define assrt_null(ptr) if (ptr) { err("Memory failure - pointer was not NULL"); errno = 0; goto error; }
 
 #define err(msg, ...) fprintf(stderr, "[ERR] [%s-%d] [errno: %s] " msg "\n", __FILE__, __LINE__, explicit_errno(), ##__VA_ARGS__)
 #define wrn(msg, ...) fprintf(stderr, "[WRN] [%s-%d] [errno: %s] " msg "\n", __FILE__, __LINE__, explicit_errno(), ##__VA_ARGS__)
