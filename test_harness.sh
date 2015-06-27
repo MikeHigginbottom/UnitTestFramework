cee2372e98435f6d91b2a4d3e5196124c3226333
Mike Higginbottom <mike@mikehigginbottom.com>
2015-06-27 18:38:30 +0100

    Only dump log file contents on test case failure

--- test_harness.sh
+++ test_harness.sh
@@ -1,3 +1,4 @@
+err=0
 log_file=results.log
 rm -f $log_file
 echo""
@@ -10,9 +11,13 @@ do
 			echo "Test suite" $suite "passed\n"
 		else
 			echo "Test suite" $suite "failed\n"
+			err=1
 		fi
 	fi
 done
-echo "Dump of" $log_file "follows:"
-more $log_file
-echo ""
+if [ $err -eq 1 ]
+then
+	echo "Dump of" $log_file "follows:"
+	more $log_file
+	echo ""
+fi
